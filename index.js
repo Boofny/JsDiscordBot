ff398a92b6de77daa87f2b95d8ed48bfd0f97027
Boofny <Brandonpizano7557@gmail.com>
2025-09-17 22:53:52 +0000

    need to make a adding pictures feature

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits, ActivityType } from 'discord.js';
+import { Client, GatewayIntentBits, ActivityType} from 'discord.js';
 import { TOKEN, TARGET_USER_ID, TARGET_USER_ID2, TARGET_USER_ID3} from './config.js';
 
 const client = new Client({
@@ -19,27 +19,35 @@ client.on('messageCreate', (message) => {
 
   if (message.author.id === TARGET_USER_ID) {
     message.channel.send(`Shut it hunter`);
-    console.log("Hunter Chatted")
+    console.log(message.author.username, "Chatted")
   }
 
   if (message.author.id === TARGET_USER_ID2) {
     message.channel.send(`SHUT IT DEIGO NOW`);
-    console.log("Deigo Chatted")
+    console.log(message.author.username, "Chatted")
   }
+  
   if (message.author.id === TARGET_USER_ID3) {
     message.channel.send(`HIIIII julio :3 <3`);
-    console.log("Julio Chatted")
+    console.log(message.author.username, "Chatted")
   }
+
+  // if (message.author.id === SELF) {
+  //   message.channel.send(`You so tuff and handsome twin!`);
+  //   console.log(message.author.username, "Chatted")
+  // }
   const content = message.content.toLowerCase()
   if (content.includes("crack") || content.includes("Crack")){
     message.channel.send("We all craking julio and hunter")
   }
+
   if (content.includes("hunt")){
     if (message.author.id === TARGET_USER_ID){
       message.channel.send("Not you hunter");
     }
     message.channel.send(`${message.author}, Is in love with hunter for his money.`);
   }
+
 });
 
 client.login(TOKEN);
