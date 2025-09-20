65783687a287cfb448ed17951d18f61f46e239aa
Boofny <Brandonpizano7557@gmail.com>
2025-09-19 23:03:16 -0400

    adding gifs sending

--- main.js
+++ main.js
@@ -19,17 +19,17 @@ client.on('messageCreate', async(message) => {
   if (message.author.bot) return;
 
   if (message.author.id === TARGET_USER_ID) {
-    message.channel.send(`Shut it hunter`);
+    // message.channel.send(`Shut it hunter`);
     console.log(message.author.username, "Chatted")
   }
 
   if (message.author.id === TARGET_USER_ID2) {
-    message.channel.send(`SHUT IT DEIGO NOW`);
+    // message.channel.send(`SHUT IT DEIGO NOW`);
     console.log(message.author.username, "Chatted")
   }
   
   if (message.author.id === TARGET_USER_ID3) {
-    message.channel.send(`HIIIII julio :3 <3`);
+    // message.channel.send(`HIIIII julio :3 <3`);
     console.log(message.author.username, "Chatted")
   }
 
@@ -42,19 +42,27 @@ client.on('messageCreate', async(message) => {
     message.channel.send("We all craking julio and hunter")
   }
 
-  if (content.includes("hunt")){
-    if (message.author.id === TARGET_USER_ID){
-      message.channel.send("Not you hunter");
-    }
-    message.channel.send(`${message.author}, Is in love with hunter for his money.`);
+  if (content.includes("hunt") && message.author.id === SELF){
+    const user = await message.guild.members.fetch(TARGET_USER_ID)
+    user.setNickname("LOSER")
+    // message.channel.send(`${message.author}, Is in love with hunter for his money.`);
   }
 
-  const pic = "https://images.pexels.com/photos/33753958/pexels-photo-33753958.jpeg";
-  if (content.includes("!pic") && message.author.id === SELFPT2){ // only those i chose
-    const user = await message.guild.members.fetch(message.author.id)
-    user.setNickname("LOSER")
+  if (content.includes("hunter") && message.author.id === SELF){
+    const user = await message.guild.members.fetch(TARGET_USER_ID)
+    user.setNickname(null)
+    // message.channel.send(`${message.author}, Is in love with hunter for his money.`);
+  }
+
+  // const pic = "https://images.pexels.com/photos/33753958/pexels-photo-33753958.jpeg";
+  const pic = "https://t4.ftcdn.net/jpg/15/68/97/83/360_F_1568978321_JVKyawq58nNqd9mekQ4EEvm8MraRvVHZ.jpg"
+  if (content.includes("!pic")){ // only those i chose
     const file = new AttachmentBuilder(pic); 
     message.channel.send({ files: [file] });
+  }
+  const gif = "https://tenor.com/view/charlie-kirk-angry-raging-american-hot-and-sweaty-gif-13127368371457359899"
+  if (message.content.includes("mad")) {
+    message.channel.send(gif);
   }
 
   if (content.includes("!test") && message.author.id === SELFPT2){ // only those i chose
@@ -62,8 +70,9 @@ client.on('messageCreate', async(message) => {
     user.setNickname(null)
   }
 
-  const banned = "https://tenor.com/view/gorilla-monke-monkey-spin-ape-gif-25078721"
-  if (message.author.id === SELFPT2 && content.includes(banned)) {
+  // const banned = "https://tenor.com/view/gorilla-monke-monkey-spin-ape-gif-25078721"
+  const banned = "https://tenor.com/view/stank-dog-smells-gif-25356900"
+  if (content.includes(banned)) { //hunter
     await message.delete();
     // Check text content
     const hasBannedLink = message.content.includes(banned);
