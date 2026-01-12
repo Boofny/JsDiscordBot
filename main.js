f44b63e8799230407e2453396ad5f69c5eb26bec
Boofny <Brandonpizano7557@gmail.com>
2026-01-12 16:45:50 -0500

    updated some return mesages

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const client = new Client({
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity('To hunter and deigo yaoi', { type: ActivityType.Listening});
+  client.user.setActivity('others', { type: ActivityType.Listening});
 });
 
 client.on('messageCreate', async(message) => {
@@ -19,39 +19,24 @@ client.on('messageCreate', async(message) => {
   if (message.author.bot) return;
 
   if (message.author.id === TARGET_USER_ID) {
-    // message.channel.send(`Shut it hunter`);
     console.log(message.author.username, "Chatted")
   }
 
   if (message.author.id === TARGET_USER_ID2) {
-    // message.channel.send(`SHUT IT DEIGO NOW`);
     console.log(message.author.username, "Chatted")
   }
   
   if (message.author.id === TARGET_USER_ID3) {
-    // message.channel.send(`HIIIII julio :3 <3`);
     console.log(message.author.username, "Chatted")
   }
 
   if (message.author.id === SELF || message.author.id === SELFPT2) {
-    // message.channel.send(`You so tuff and handsome twin!`);
     console.log(message.author.username, "Chatted")
   }
   const content = message.content.toLowerCase()
-  if (content.includes("crack") || content.includes("Crack")){
-    message.channel.send("We all craking julio and hunter")
-  }
 
   if (content.includes("hunt") && message.author.id === SELF){
     const user = await message.guild.members.fetch(TARGET_USER_ID)
-    user.setNickname("LOSER")
-    // message.channel.send(`${message.author}, Is in love with hunter for his money.`);
-  }
-
-  if (content.includes("hunter") && message.author.id === SELF){
-    const user = await message.guild.members.fetch(TARGET_USER_ID)
-    user.setNickname(null)
-    // message.channel.send(`${message.author}, Is in love with hunter for his money.`);
   }
 
   // const pic = "https://images.pexels.com/photos/33753958/pexels-photo-33753958.jpeg";
@@ -59,10 +44,6 @@ client.on('messageCreate', async(message) => {
   if (content.includes("!pic")){ // only those i chose
     const file = new AttachmentBuilder(pic); 
     message.channel.send({ files: [file] });
-  }
-  const gif = "https://tenor.com/view/charlie-kirk-angry-raging-american-hot-and-sweaty-gif-13127368371457359899"
-  if (message.content.includes("mad")) {
-    message.channel.send(gif);
   }
 
   if (content.includes("!test") && message.author.id === SELFPT2){ // only those i chose
